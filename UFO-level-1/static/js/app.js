0da79bd2f0b202968337415a6154705ef3685558
Riley Fritz <rileyfritz5773@gmail.com>
2021-04-13 10:45:55 -0500

    all files setup. working on function to put all data in html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,26 @@
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+  }
+
 // from data.js
 var tableData = data;
+// var datetimes = unpack(datetime, 0);
 
-// YOUR CODE HERE!
+console.log(data[0].datetime);
+
+// function buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, volume) {
+//     var table = d3.select("#summary-table");
+//     var tbody = table.select("tbody");
+//     var trow;
+//     for (var i = 0; i < 12; i++) {
+//       trow = tbody.append("tr");
+//       trow.append("td").text(dates[i]);
+//       trow.append("td").text(openPrices[i]);
+//       trow.append("td").text(highPrices[i]);
+//       trow.append("td").text(lowPrices[i]);
+//       trow.append("td").text(closingPrices[i]);
+//       trow.append("td").text(volume[i]);
+//     }
+//   }
